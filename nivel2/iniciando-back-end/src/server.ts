8640cb65170b300751a14162279e2ad13d5106e7
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-02-03 12:07:16 -0300

    avatar cont

--- nivel2/iniciando-back-end/src/server.ts
+++ nivel2/iniciando-back-end/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 import 'reflect-metadata';
 
 import './database';
@@ -7,8 +8,9 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
-    console.log('server started.')
+    console.log('🚀 Server started on port 3333');
 });
