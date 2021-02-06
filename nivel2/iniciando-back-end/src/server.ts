cbac3086a9b5637249c14849eb029f0da7f1635a
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-02-06 20:54:26 -0300

    Habilitando CORS na API

--- nivel2/iniciando-back-end/src/server.ts
+++ nivel2/iniciando-back-end/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -10,6 +11,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -20,9 +22,9 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
         .status(err.statusCode)
         .json({ status: 'error', message: err.message });
     }
-  
+
     console.error(err);
-  
+
     return response
       .status(500)
       .json({ status: 'error', message: 'Internal server error' });
