8b06003414ecc8db50cbf535c7114c6a46d9866a
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-02-06 20:41:51 -0300

    Validação de login

--- nivel3/gobarberapp/src/App.tsx
+++ nivel3/gobarberapp/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+       <SignIn />
       <GlobalStyle />
     </>
   );
