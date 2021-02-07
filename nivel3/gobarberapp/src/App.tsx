7709645d687b59a62fe066fbc1b52ebac7fa28dd
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-02-06 22:41:09 -0300

    Login pelo Contexto, hooks, context api

--- nivel3/gobarberapp/src/App.tsx
+++ nivel3/gobarberapp/src/App.tsx
@@ -3,10 +3,15 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
-       <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+
       <GlobalStyle />
     </>
   );
