c6887457b55afe69d74e13189f85a3f23d0d316c
Carlos Renato Simões <carlos.renato.simoes@gmail.com>
2021-02-08 23:16:14 -0300

    Rotas autenticadas

--- nivel3/gobarberapp/src/components/ToastContainer/Toast/index.tsx
+++ nivel3/gobarberapp/src/components/ToastContainer/Toast/index.tsx
@@ -38,7 +38,7 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
     type={message.type}
     hasDescription={!!message.description}
     style={style}
-  >
+    >
       {icons[message.type || 'info']}
       <div>
         <strong>{message.title}</strong>
